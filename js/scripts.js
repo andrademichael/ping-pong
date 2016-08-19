f3739e5c3b842c1ab5b7d902ff3d851db67ef992
michael <andrade.michael.e@gmail.com>
2016-08-19 15:38:59 -0700

    Final commit before submitting, cleaned up spacing, deleted superfluous variables, tested that it still all runs

--- js/scripts.js
+++ js/scripts.js
@@ -1,52 +1,30 @@
 $(document).ready(function() {
 //Backend
-
   var countUp = function(number) {
-      var numbers = [];
       var arrayOutput = [];
-      for (var i = 0; i <= number - 1; i++) {
+      for (var i = 0; i < number; i++) {
         if ((i + 1) % 15 === 0) {
           arrayOutput[i] = "Ping...PONG!";
         } else if ((i + 1) % 5 === 0) {
           arrayOutput[i] = "Pong.";
-        }
-          else if ((i + 1) % 3 === 0) {
-            arrayOutput[i] = "Ping!";
+        } else if ((i + 1) % 3 === 0) {
+          arrayOutput[i] = "Ping!";
         } else {
           arrayOutput[i] = [1 + i];
         }
       }
-      console.log(numbers);
       return arrayOutput;
-      console.log("countUp ran");
     };
-
-  // var pingPonger = function(arrayInput) {
-  //   var arrayOutput = arrayInput.slice(0);
-  //   console.log(arrayInput);
-  //   for (var i = 0; i < arrayInput.length; i++) {
-  //     if (arrayInput[i] % 15 === 0) {
-  //       arrayOutput[i] = "ping...PONG!";
-  //     } elseif {
-  //
-  //     }
-  //   }
-  //   return arrayOutput;
-  // };
-
 //Frontend
   $("#inputForm").submit(function(event) {
     $("div").removeClass("has-error");
     event.preventDefault();
     $("#inputBox").removeClass("has-error");
     $("#inputHelp").text("");
-
     var inputNum = parseInt($("#inputBox").val());
-
     if(!inputNum) {
       $("#inputBox").addClass("has-error");
       $("#inputHelp").text("...Please, enter a whole number?");
-      console.log(inputNum);
     } else {
         var output = countUp(inputNum);
         $("#outputList").text("");
@@ -56,7 +34,6 @@ $(document).ready(function() {
         $("#outputSentence").text("Ok, here's the serve with " + inputNum + " volleys!");
         $("#outputRhetoric").text("Isn't this an exciting game?")
         $("#num").text(inputNum);
-        console.log("form submit ran with input " + inputNum);
     };
   });
 });
