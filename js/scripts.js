2294e0f07b6208eb290d721bfd284fd5b040c926
michael <andrade.michael.e@gmail.com>
2016-08-19 11:30:37 -0700

    a

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,19 @@
 $(document).ready(function() {
 
-  //Frontend
+//Frontend
   $("form").submit(function(event) {
     $("div").removeClass("has-error");
     event.preventDefault();
     var input = $("#nameInput").val();
     var output = answerParser(input);
 
-    //
-    var answerParser = function(name)
-    if(!name) {
-      $("#numBox").addClass("has-error");
-      $("#numHelp").text("Please enter a whole number");
-    } else {
+//Backend
+  var answerParser = function(name)
+  if(!name) {
+    $("#numBox").addClass("has-error");
+    $("#numHelp").text("Please enter a whole number");
+  } else {
 
-      console.log(ruby, java, csharp, design)
-      $("img").hide();
-      $("div#trackSuggestion").css("backgroundColor", "favColor");
-      $("#trackSuggestion").show();
-      $("#nameOut").text(name);
-
-      //Which result to display logic
-    }
+  }
   });
 });
