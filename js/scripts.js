58a3f94d85a5a086d20fb01238bc59f699553019
michael <andrade.michael.e@gmail.com>
2016-08-19 14:14:26 -0700

    spec 1: display the numbers from 1 to input

--- js/scripts.js
+++ js/scripts.js
@@ -8,31 +8,39 @@ $(document).ready(function() {
     $("#inputHelp").text("");
 
     var inputNum = parseInt($("#inputBox").val());
-    var pingParser = function(pingInput) {
 
-    };
     if(!inputNum) {
       $("#inputBox").addClass("has-error");
       $("#inputHelp").text("...Please, enter a whole number?");
       console.log(inputNum);
     } else {
-      var output = pingParser(inputNum);
-
-    $("#outputDisplay").show;
-    console.log("main thing ran");
+      var output = pingPonger(inputNum);
+    //   for (var i = 0; i < (output).length; i++) {
+    // $("#outputDisplay").text(output[i]);
+    //   }
+    $("#outputDisplay").text(output);
+    $("#num").text(inputNum);
+    console.log("main thing ran with input " + inputNum);
     };
   });
 
 //Backend
-  var answerParser = function(name) {
-  //
-  // var func = function() {
-  //
-  // };
-  // if((single int being passed from array)% 3 === 0) {
-  //    (sibpfa) = "ping."
-  //    return that
 
-  // };
+  var countUp = function(number) {
+      var numbers = [];
+      for (var i = 0; i < number; i++) {
+        numbers.push(1 + i);
+      }
+      return numbers;
+      console.log("countUp ran");
+      console.log(numbers);
+    };
+
+  var pingPonger = function(arrayInput) {
+    return countUp(arrayInput);
+    // if((single int being passed from array)% 3 === 0) {
+    //   (sibpfa) = "ping."
+    //   return that;
+    // }
   };
 });
